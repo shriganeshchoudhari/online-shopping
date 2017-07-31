1f8d32e0b876b3bf5efdb06235faa324e185d0c0
SHRIGANESH <SHRIGANESH@SHRIGANESH-PC>
2017-07-31 15:03:07 +0530

    complated the interface

--- onlineshopping/src/main/java/net/km/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/net/km/onlineshopping/controller/PageController.java
@@ -10,8 +10,24 @@ public class PageController {
 	public ModelAndView index() {
 
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting", "welcome to spring web mvc");
+		mv.addObject("title", "Home");
+		mv.addObject("userClickHome", true);
 		return mv;
 	}
+	@RequestMapping(value = {"/about" })
+	public ModelAndView about() {
 
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "About");
+		mv.addObject("userClickAbout", true);
+		return mv;
+	}
+	@RequestMapping(value = { "/contact" })
+	public ModelAndView contact() {
+
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "Contact");
+		mv.addObject("userClickContact", true);
+		return mv;
+	}
 }
---
+++ onlineshopping/src/main/webapp/assets/js/myapp.js
@@ -0,0 +1,16 @@
+$(function() {
+	// solving the active menu problem
+	switch(menu) {
+	
+		case 'About':
+			$('#about').addClass('active');
+			break;
+		case 'Contact':
+			$('#contact').addClass('active');
+			break;
+		
+		default:	
+			$('#home').addClass('active');
+			break;		
+	}
+});
